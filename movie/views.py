ec4b8b62e7de2dc6d59e0d64f25bbea2a6e1e953
Alexander Ratelle <alexander.ratelle@gmail.com>
2021-10-15 21:00:18 -0400

    Made a working OMDB request

--- movie/views.py
+++ movie/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 import os
+import omdb
 
 def getsecret(source):
     with open(os.getenv(source)) as keyfile:
         return keyfile.read().rstrip('\n')
 
-OMDB_KEY = getsecret('OMDB_KEY_FILE')
+omdb.set_default('apikey', getsecret('OMDB_KEY_FILE'))
 
 # Create your views here.
 
 def search(request):
-    return render(request, 'movie/search.html', {'query': request.GET['q']})
+    title = request.GET['q']
+    print(omdb.search_movie(title))
+    return render(request, 'movie/search.html', {'query': title})

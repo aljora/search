a0af6069e4e62216dc7228743efe8b66e60d31fe
Alexander Ratelle <alexander.ratelle@gmail.com>
2021-10-15 20:34:28 -0400

    Added secret OMDB key

--- movie/views.py
+++ movie/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+import os
+
+def getsecret(source):
+    with open(os.getenv(source)) as keyfile:
+        return keyfile.read().rstrip('\n')
+
+OMDB_KEY = getsecret('OMDB_KEY_FILE')
 
 # Create your views here.
 
